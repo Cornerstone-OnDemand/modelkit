b876666121f0716b78e2b1366b58d219fd4c4f23
Victor Benichoux <vbenichoux@csod.com>
2021-06-04 15:26:30 +0200

    model: also use new caching logic for async models

--- modelkit/core/model.py
+++ modelkit/core/model.py
@@ -564,93 +564,81 @@ class AsyncModel(BaseModel[ItemType, ReturnType]):
         self,
         items: Iterator[ItemType],
         batch_size: int = None,
-        _force_compute: bool = False,
         _callback: Callable = None,
+        _force_compute: bool = False,
         **kwargs,
     ) -> AsyncIterator[ReturnType]:
         batch_size = batch_size or self.batch_size
+
         batch: List[ItemType] = []
+        cached_results: List[Union[ItemType, CachedStatus]] = []
         step = 0
         while True:
             try:
                 if len(batch) == batch_size:
-                    async for r in self._predict_single_batch_gen(
+                    async for r in self._predict_from_batch_and_cache(
                         step,
                         batch,
-                        _force_compute=_force_compute,
+                        cached_results,
                         _callback=_callback,
                         **kwargs,
                     ):
                         yield r
-                    step += batch_size
+
                     batch = []
+                    cached_results = []
+                    step += batch_size
                 else:
-                    batch.append(next(items))
+                    current_item = self._validate(
+                        next(items), self._item_model, ItemValidationException
+                    )
+                    if (
+                        self.redis_cache
+                        and self.model_settings.get("cache_predictions")
+                        and not _force_compute
+                    ):
+                        cached_results.append(
+                            self.redis_cache.get(current_item, CachedStatus.NOT_CACHED)
+                        )
+                    else:
+                        cached_results.append(CachedStatus.NOT_CACHED)
+                    batch.append(current_item)
             except StopIteration:
                 break
+
         if batch:
-            async for r in self._predict_single_batch_gen(
+            async for r in self._predict_from_batch_and_cache(
                 step,
                 batch,
-                _force_compute=_force_compute,
+                cached_results,
                 _callback=_callback,
                 **kwargs,
             ):
                 yield r
 
-    async def _predict_single_batch_gen(
-        self,
-        _step: int,
-        items: List[ItemType],
-        _force_compute: bool = False,
-        _callback: Callable = None,
-        **kwargs,
+    async def _predict_from_batch_and_cache(
+        self, _step: int, batch, cached_results, _callback: Callable = None, **kwargs
     ) -> AsyncIterator[ReturnType]:
-        items = [
-            self._validate(i, self._item_model, ItemValidationException) for i in items
-        ]
-        if self.redis_cache and self.model_settings.get("cache_predictions"):
-            # In the case where cache is activated, sieve through
-            # individual items
-            results = []
-            to_compute = []
-            for kitem, item in enumerate(items):
-                key = self.item_cache_key(item, kwargs)
-                if not _force_compute and self.redis_cache.exists(key):
-                    # We trust the data coming from Redis as it's a local cache
-                    unpickled = pickle.loads(self.redis_cache.get(key))  # nosec
-                    results.append(unpickled)
-                else:
-                    results.append(None)
-                    to_compute.append((kitem, key, item))
-            computed_results = await self._predict_batch(
-                [item[2] for item in to_compute],
-                **kwargs,
-            )
-            for ((kitem, key, _), result) in zip(to_compute, computed_results):
-                self.redis_cache.set(key, pickle.dumps(result))
-                results[kitem] = result
-            logger.debug(
-                "Caching digest",
-                recomputed=len(computed_results),
-                from_cache=(len(results) - len(computed_results)),
-                model=self.configuration_key,
-            )
-        else:
-            # general case: items is a list of items to treat
-            # if there are multiple examples but no batching
-            # or if there are multiple examples and batching
-            results = [
-                self._validate(r, self._return_model, ReturnValueValidationException)
-                for r in await self._predict_batch(
-                    items,
-                    **kwargs,
+        predictions = await self._predict_batch(batch, **kwargs)
+        items_and_predictions = zip(batch, predictions)
+        for res in cached_results:
+            if res == CachedStatus.NOT_CACHED:
+                item, r = next(items_and_predictions)
+                if self.redis_cache and self.model_settings.get("cache_predictions"):
+                    self.redis_cache.setdefault(item, r)
+                yield self._validate(
+                    r,
+                    self._return_model,
+                    ReturnValueValidationException,
                 )
-            ]
+            else:
+                yield self._validate(
+                    res,
+                    self._return_model,
+                    ReturnValueValidationException,
+                )
         if _callback:
-            _callback(_step, items, results)
-        for r in results:
-            yield r
+            _callback(_step, batch, predictions)
 
 
 class WrappedAsyncModel:
