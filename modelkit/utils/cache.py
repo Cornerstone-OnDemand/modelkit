5de1b5062fcff05fa13fea4cf707d5afc4fe328a
Victor Benichoux <vbenichoux@csod.com>
2021-06-07 08:44:26 +0200

    caching: add type annotations

--- modelkit/utils/cache.py
+++ modelkit/utils/cache.py
@@ -1,16 +1,21 @@
 import hashlib
 import pickle
-from typing import Any, Dict
+from typing import Any, Dict, Generic, Optional
 
 import pydantic
 
 import modelkit
+from modelkit.core.types import ItemType
 from modelkit.utils.redis import connect_redis
 
 
-class CacheItem:
+class CacheItem(Generic[ItemType]):
     def __init__(
-        self, item=None, cache_key=None, cache_value=None, missing: bool = True
+        self,
+        item: Optional[ItemType] = None,
+        cache_key: bytes = None,
+        cache_value=None,
+        missing: bool = True,
     ):
         self.item = item
         self.cache_key = cache_key
@@ -23,7 +28,7 @@ class RedisCache:
         self.redis = connect_redis(host, port)
         self.cache_keys = {}
 
-    def hash_key(self, model_key, item: Any, kwargs: Dict[str, Any]):
+    def hash_key(self, model_key: str, item: Any, kwargs: Dict[str, Any]):
         cache_key = self.cache_keys.get(model_key)
         if not cache_key:
             self.cache_keys[model_key] = (model_key + modelkit.__version__).encode()
@@ -31,7 +36,7 @@ class RedisCache:
         pickled = pickle.dumps((item, kwargs))  # nosec: only used to build a hash
         return hashlib.sha256(cache_key + pickled).digest()
 
-    def get(self, model_key, item, kwargs):
+    def get(self, model_key: str, item: Any, kwargs: Dict[str, Any]):
         cache_key = self.hash_key(model_key, item, kwargs)
         r = self.redis.get(cache_key)
         if r is None:
@@ -40,7 +45,7 @@ class RedisCache:
             cache_key=cache_key, cache_value=pickle.loads(r), missing=False
         )
 
-    def set(self, k, d):
+    def set(self, k: bytes, d: Any):
         if isinstance(d, pydantic.BaseModel):
             self.redis.set(k, pickle.dumps(d.dict()))
         else:
