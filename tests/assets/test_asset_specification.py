6e0b17132b634125bb747d0f64e7cec3a3acf21a
Victor Benichoux <vbenichoux@csod.com>
2021-06-09 14:42:01 +0200

    assets specs: use correct separator for windows

--- tests/assets/test_asset_specification.py
+++ tests/assets/test_asset_specification.py
@@ -17,7 +17,7 @@ from modelkit.assets.settings import AssetSpec
         ("some_go/0d_name", True),
         ("SOME_GOOD_NAME_AS_WELL", True),
         ("50M3_G00D_N4ME_4S_W3LL", True),
-        ("C:/A/L0cAL/Windows/file.ext", True),
+        ("C:\\A\\L0cAL\\Windows\\file.ext", True),
     ],
 )
 def test_names(test, valid):
@@ -142,9 +142,9 @@ TEST_STRING_SPECS = [
     ),
     ("blabli/blebla[foo]", {"name": "blabli/blebla", "sub_part": "foo"}),
     (
-        "C:/A/L0cAL/Windows/file.ext",
+        "C:\A\L0cAL\Windows\\file.ext",
         {
-            "name": "C:/A/L0cAL/Windows/file.ext",
+            "name": "C:\A\L0cAL\Windows\\file.ext",
             "sub_part": None,
             "major_version": None,
             "minor_version": None,
