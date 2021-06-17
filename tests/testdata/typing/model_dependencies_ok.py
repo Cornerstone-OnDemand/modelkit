bf967b8c78b073a33e9a4624ae2fddea88432d0b
Victor Benichoux <vbenichoux@csod.com>
2021-06-17 22:22:42 +0200

    model: add a custom dependency dictionary

---
+++ tests/testdata/typing/model_dependencies_ok.py
@@ -0,0 +1,28 @@
+from typing import Any, Dict
+
+from modelkit.core.library import ModelLibrary
+from modelkit.core.model import Model
+
+
+class SomeModel(Model[str, str]):
+    CONFIGURATIONS: Dict[str, Any] = {"dependent": {}}
+
+    def _predict(self, item):
+        return item
+
+
+class SomeOtherModel(Model[str, str]):
+    CONFIGURATIONS: Dict[str, Any] = {
+        "something": {"model_dependencies": {"dependent"}}
+    }
+
+    def _predict(self, item):
+        m = self.model_dependencies.get("dependent", SomeModel)
+        res = m.predict(item)
+        return res
+
+
+lib = ModelLibrary(models=[SomeModel, SomeOtherModel])
+
+m2 = lib.get("something", model_type=SomeOtherModel)
+m2.predict("str")
