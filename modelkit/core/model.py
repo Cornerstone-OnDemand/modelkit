86241fa1ea655ad41a250326bdc36c90ead5c7a7
Victor Benichoux <victor.benichoux@gmail.com>
2021-06-18 15:02:36 +0200

    Update modelkit/core/model.py
    Co-authored-by: Antoine Jeannot <71255023+antoinejeannot@users.noreply.github.com>

--- modelkit/core/model.py
+++ modelkit/core/model.py
@@ -384,7 +384,7 @@ class AbstractModel(Asset, Generic[ItemType, ReturnType]):
             self._predict_mode = PredictMode.SINGLE
         if not hasattr(self._predict_batch, "__not_overriden__"):
             if self._predict_mode == PredictMode.SINGLE:
-                raise BothPredictsOverridenError()
+                raise BothPredictsOverridenError("_predict OR _predict_batch must be overriden, not both")
             self._predict_mode = PredictMode.BATCH
         if not self._predict_mode:
             raise NoPredictOverridenError("_predict or _predict_batch must be overriden")
