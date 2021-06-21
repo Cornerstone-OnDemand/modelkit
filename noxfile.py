9cba8103606a24aac7d129e17c19c8960981d120
Antoine Jeannot <ajeannot@csod.com>
2021-06-21 14:37:10 +0200

    readme: add badges

--- noxfile.py
+++ noxfile.py
@@ -6,20 +6,24 @@ def test(session):
     # Install deps and the package itself.
     session.install("-r", "requirements-dev.txt")
 
-    session.run(
-        "pytest"
-    )
+    session.run("pytest", "--junitxml=junit.xml")
+    session.install("genbadge[tests]")
+    session.run("genbadge", "tests", "-i", "junit.xml", "-o", "docs/badges/tests.svg")
+
 
 @nox.session(python=["3.7"])
 def coverage(session):
     # Install deps and the package itself.
     session.install("-r", "requirements-optional.txt")
 
-    session.run(
-        "coverage",
-        "run",
-        "-m",
-        "pytest"
-    )
+    session.run("coverage", "run", "-m", "pytest")
     session.run("coverage", "report", "-m")
-    session.run("coverage", "xml")
+    session.install("genbadge[coverage]")
+    session.run(
+        "genbadge",
+        "coverage",
+        "-i",
+        "coverage.xml",
+        "-o",
+        "docs/badges/coverage.svg",
+    )
