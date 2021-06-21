a2fd54741469ca57e10a72081d4d2fa47cb333d6
Quentin Pradet <quentin.pradet@gmail.com>
2021-06-21 10:18:33 +0400

    Fix lint
    https://numpy.org/doc/stable/release/1.20.0-notes.html#using-the-aliases-of-builtin-types-like-np-int-is-deprecated

--- modelkit/core/model.py
+++ modelkit/core/model.py
@@ -345,10 +345,14 @@ class AbstractModel(Asset, Generic[ItemType, ReturnType]):
             self._predict_mode = PredictMode.SINGLE
         if not hasattr(self._predict_batch, "__not_overriden__"):
             if self._predict_mode == PredictMode.SINGLE:
-                raise BothPredictsOverridenError("_predict OR _predict_batch must be overriden, not both")
+                raise BothPredictsOverridenError(
+                    "_predict OR _predict_batch must be overriden, not both"
+                )
             self._predict_mode = PredictMode.BATCH
         if not self._predict_mode:
-            raise NoPredictOverridenError("_predict or _predict_batch must be overriden")
+            raise NoPredictOverridenError(
+                "_predict or _predict_batch must be overriden"
+            )
 
 
 class CallableWithAttribute(Protocol):
