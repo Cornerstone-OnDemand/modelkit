efb816a153f0d6194fbf6925df864a796689e89c
Victor Benichoux <vbenichoux@csod.com>
2021-06-21 17:52:37 +0200

    library: also test multiple asset load

--- tests/test_core.py
+++ tests/test_core.py
@@ -514,3 +514,34 @@ def test_model_multiple_load():
     lib.get("b")
     lib.get("a")
     assert loaded == 1
+
+
+def test_model_multiple_asset_load(working_dir, monkeypatch):
+    monkeypatch.setenv("MODELKIT_ASSETS_DIR", working_dir)
+    with open(os.path.join(working_dir, "something.txt"), "w") as f:
+        f.write("OK")
+
+    class SomeModel(Model):
+        CONFIGURATIONS = {"a": {"asset": "something.txt"}}
+
+        def _predict(self, item):
+            return item
+
+    class SomeModel2(Model):
+        CONFIGURATIONS = {"b": {"asset": "something.txt"}}
+
+        def _predict(self, item):
+            return item
+
+    fetched = 0
+
+    def fake_fetch_asset(asset_spec, return_info=True):
+        nonlocal fetched
+        fetched += 1
+        return {"path": os.path.join(working_dir, "something.txt")}
+
+    lib = ModelLibrary(models=[SomeModel, SomeModel2], settings={"lazy_loading": True})
+    monkeypatch.setattr(lib.asset_manager, "fetch_asset", fake_fetch_asset)
+    lib.preload()
+
+    assert fetched == 1
