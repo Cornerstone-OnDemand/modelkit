b63648d39537d788607d47553c106935576d22f4
Victor Benichoux <vbenichoux@csod.com>
2021-06-24 14:12:25 +0200

    memory: add pragma no cover on things that are tested only on windows

--- modelkit/utils/memory.py
+++ modelkit/utils/memory.py
@@ -14,14 +14,18 @@ class PerformanceTracker:
 
     def __enter__(self):
         self.start_time = time.perf_counter()
-        if platform.system() == "Windows":
+        # the `Windows` case is tested, but not that
+        # from which coverage is gathered (it runs on Ubuntu)
+        if platform.system() == "Windows":  # pragma: no cover
             return self
         self.pre_maxrss_bytes = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         return self
 
     def __exit__(self, *args):
         self.time = time.perf_counter() - self.start_time
-        if platform.system() == "Windows":
+        # the `Windows` case is tested, but not that
+        # from which coverage is gathered (it runs on Ubuntu)
+        if platform.system() == "Windows":  # pragma: no cover
             return
         post_maxrss_bytes = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         self.increment = post_maxrss_bytes - self.pre_maxrss_bytes
