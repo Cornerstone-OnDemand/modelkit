94f3dce9d465fb43637b2fc9fc9846df6f21e3c0
Victor Benichoux <vbenichoux@csod.com>
2021-06-24 15:31:05 +0200

    memory: add pragma no cover on imports that fail on windows

--- modelkit/utils/memory.py
+++ modelkit/utils/memory.py
@@ -4,7 +4,9 @@ import time
 # 'resource' isn't supported on Windows
 try:
     import resource
-except ModuleNotFoundError:
+except ModuleNotFoundError:  # pragma: no cover
+    # the `Windows` case is tested, but not that
+    # from which coverage is gathered which runs on ubuntu
     pass
 
 
@@ -15,7 +17,7 @@ class PerformanceTracker:
     def __enter__(self):
         self.start_time = time.perf_counter()
         # the `Windows` case is tested, but not that
-        # from which coverage is gathered (it runs on Ubuntu)
+        # from which coverage is gathered which runs on ubuntu
         if platform.system() == "Windows":  # pragma: no cover
             return self
         self.pre_maxrss_bytes = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
@@ -24,7 +26,7 @@ class PerformanceTracker:
     def __exit__(self, *args):
         self.time = time.perf_counter() - self.start_time
         # the `Windows` case is tested, but not that
-        # from which coverage is gathered (it runs on Ubuntu)
+        # from which coverage is gathered which runs on ubuntu
         if platform.system() == "Windows":  # pragma: no cover
             return
         post_maxrss_bytes = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
