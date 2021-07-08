680f9b336666a44d646f3af53b9204f532417d92
Victor Benichoux <vbenichoux@csod.com>
2021-07-08 16:08:36 +0200

    tests: add test of AsyncModel.test

--- tests/test_auto_testing.py
+++ tests/test_auto_testing.py
@@ -3,7 +3,7 @@ from typing import Dict
 import pydantic
 import pytest
 
-from modelkit.core.model import Model
+from modelkit.core.model import AsyncModel, Model
 from modelkit.testing import modellibrary_auto_test, modellibrary_fixture
 
 
@@ -114,6 +114,32 @@ def test_auto_test_function(test_case, do_raise):
         TEST_CASES = [test_case]
 
         def _predict(self, item: bool, force_true=False, **_) -> bool:
+            if force_true:
+                return True
+            return item
+
+    if do_raise:
+        with pytest.raises(AssertionError):
+            MyModel().test()
+    else:
+        MyModel().test()
+
+
+@pytest.mark.parametrize(
+    "test_case, do_raise",
+    [
+        ({"item": True, "result": True}, False),
+        ({"item": False, "result": True}, True),
+        ({"item": False, "keyword_args": {"force_true": True}, "result": True}, False),
+        ({"item": False, "keyword_args": {"force_true": False}, "result": True}, True),
+    ],
+)
+def test_auto_test_function_async(test_case, do_raise):
+    class MyModel(AsyncModel):
+        CONFIGURATIONS = {"my_model": {}}
+        TEST_CASES = [test_case]
+
+        async def _predict(self, item: bool, force_true=False, **_) -> bool:
             if force_true:
                 return True
             return item
