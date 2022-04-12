bd119f22f88879e12c251ff9aebfcba7719e9b5e
Cyril Le Mat <clemat@csod.com>
2022-04-12 10:15:49 +0200

    fix describe variables

--- modelkit/core/model.py
+++ modelkit/core/model.py
@@ -344,10 +344,10 @@ class AbstractModel(Asset, Generic[ItemType, ReturnType]):
     def _compute_dependencies_load_info(self):
         global_load_info = {}
         add_dependencies_load_info(global_load_info, self)
-        global_load_time = (self._load_memory_increment or 0) + sum(
+        global_load_memory = (self._load_memory_increment or 0) + sum(
             x["memory_increment"] for x in global_load_info.values()
         )
-        global_load_memory = (self._load_time or 0) + sum(
+        global_load_time = (self._load_time or 0) + sum(
             x["time"] for x in global_load_info.values()
         )
         return global_load_time, global_load_memory
