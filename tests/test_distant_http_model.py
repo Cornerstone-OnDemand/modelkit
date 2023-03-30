a64cff94cebcd4c0f894144c96897e011d862afb
Antoine Jeannot <ajeannot@csod.com>
2023-03-30 15:31:02 +0200

    Implement async distant model with predict batch

--- tests/test_distant_http_model.py
+++ tests/test_distant_http_model.py
@@ -8,6 +8,7 @@ import requests
 
 from modelkit.core.library import ModelLibrary
 from modelkit.core.models.distant_model import (
+    AsyncDistantHTTPBatchModel,
     AsyncDistantHTTPModel,
     DistantHTTPBatchModel,
     DistantHTTPModel,
@@ -107,6 +108,7 @@ async def test_distant_http_model(
         assert expected == m(item, endpoint_params=params)
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "items, params, expected",
     [
@@ -141,28 +143,52 @@ async def test_distant_http_model(
         ),
     ],
 )
-def test_distant_http_batch_model(
+async def test_distant_http_batch_model(
     items, params, expected, run_mocked_service, event_loop
 ):
-    model_settings = {
+    sync_model_settings = {
         "endpoint": "http://127.0.0.1:8000/api/path/endpoint/batch",
         "async_mode": False,
     }
+    async_model_settings = {
+        "endpoint": "http://127.0.0.1:8000/api/path/endpoint/batch",
+        "async_mode": True,
+    }
 
-    class SomeDistantHTTPBatchModel(DistantHTTPBatchModel):
+    class SomeSyncDistantHTTPBatchModel(DistantHTTPBatchModel):
         CONFIGURATIONS = {
-            "some_model_batch": {"model_settings": model_settings},
+            "some_model_sync_batch": {"model_settings": sync_model_settings},
         }
 
-    lib_without_params = ModelLibrary(models=[SomeDistantHTTPBatchModel])
+    class SomeAsyncDistantHTTPModel(AsyncDistantHTTPBatchModel):
+        CONFIGURATIONS = {
+            "some_model_async_batch": {"model_settings": async_model_settings}
+        }
+
+    lib_without_params = ModelLibrary(
+        models=[SomeSyncDistantHTTPBatchModel, SomeAsyncDistantHTTPModel]
+    )
     lib_with_params = ModelLibrary(
-        models=[SomeDistantHTTPBatchModel],
+        models=[SomeSyncDistantHTTPBatchModel, SomeAsyncDistantHTTPModel],
         configuration={
-            "some_model_batch": {
-                "model_settings": {**params, **model_settings},
-            }
+            "some_model_sync_batch": {
+                "model_settings": {**params, **sync_model_settings},
+            },
+            "some_model_async_batch": {
+                "model_settings": {**params, **async_model_settings}
+            },
         },
     )
     for lib in [lib_without_params, lib_with_params]:
-        m = lib.get("some_model_batch")
+        # Test with asynchronous mode
+        m = lib.get("some_model_async_batch")
+        with pytest.raises(AssertionError):
+            assert expected == m.predict_batch(items, endpoint_params=params)
+
+        res = await m.predict_batch(items, endpoint_params=params)
+        assert expected == res
+        await lib.aclose()
+
+        # Test with synchronous mode
+        m = lib.get("some_model_sync_batch")
         assert expected == m.predict_batch(items, endpoint_params=params)
