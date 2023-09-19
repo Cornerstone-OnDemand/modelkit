0b5be8ac033bbebe1da438d7cf7812d2e522b284
Antoine Jeannot <ajeannot@csod.com>
2023-09-19 16:19:04 +0200

    define TF Keras helpers in try..except block
    Add dummy classes if tensorflow is not installed
    
    Hide tf keras in get_keras_model

--- modelkit/core/models/tensorflow_model.py
+++ modelkit/core/models/tensorflow_model.py
@@ -87,37 +87,6 @@ class TensorflowModelMixin(abc.ABC):
         return results
 
 
-class LayerFromSavedModel(tf.keras.layers.Layer):
-    """
-    Builds a keras Layer from a saved Tensorflow model
-    """
-
-    def __init__(self, saved_model_signature):
-        super().__init__()
-        self.saved_model_signature = saved_model_signature
-
-    def call(self, inputs):
-        return self.saved_model_signature(
-            **{key: tf.convert_to_tensor(value) for key, value in inputs.items()}
-        )
-
-
-class KerasModelFromSavedModel(tf.keras.Model):
-    """
-    Builds a keras Model from a saved Tensorflow model
-    via an artificial keras Layer built from the saved model
-    signature.
-    """
-
-    def __init__(self, saved_model_signature):
-        super().__init__()
-        self.layer = LayerFromSavedModel(saved_model_signature)
-
-    def call(self, inputs):
-        out = self.layer(inputs)
-        return out
-
-
 class TensorflowModel(Model[ItemType, ReturnType], TensorflowModelMixin):
     def __init__(
         self,
@@ -270,6 +239,38 @@ class TensorflowModel(Model[ItemType, ReturnType], TensorflowModelMixin):
         }
 
     def get_keras_model(self):
+        class LayerFromSavedModel(tf.keras.layers.Layer):
+            """
+            Builds a keras Layer from a saved Tensorflow model
+            """
+
+            def __init__(self, saved_model_signature):
+                super().__init__()
+                self.saved_model_signature = saved_model_signature
+
+            def call(self, inputs):
+                return self.saved_model_signature(
+                    **{
+                        key: tf.convert_to_tensor(value)
+                        for key, value in inputs.items()
+                    }
+                )
+
+        class KerasModelFromSavedModel(tf.keras.Model):
+            """
+            Builds a keras Model from a saved Tensorflow model
+            via an artificial keras Layer built from the saved model
+            signature.
+            """
+
+            def __init__(self, saved_model_signature):
+                super().__init__()
+                self.layer = LayerFromSavedModel(saved_model_signature)
+
+            def call(self, inputs):
+                out = self.layer(inputs)
+                return out
+
         return KerasModelFromSavedModel(self.tf_model_signature)
 
     def close(self):
