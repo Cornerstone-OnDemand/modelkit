38e5c15606ae0d57c5634ef5c02ed6eaacf2f4bd
Mathilde Léval <9384853+mathilde-leval@users.noreply.github.com>
2023-09-20 11:56:05 +0200

    Add get_keras_model to TensorflowModel (#187)
    * Add get_keras_model to TensorflowModel
    
    * install optional dependencies in test session
    
    * apply lint
    
    * Revert "install optional dependencies in test session"
    
    This reverts commit de67e9fc0289276e5790a60edbadd81cf08ff723.
    
    * define TF Keras helpers in try..except block
    
    Add dummy classes if tensorflow is not installed
    
    Hide tf keras in get_keras_model
    
    ---------
    
    Co-authored-by: Antoine Jeannot <ajeannot@csod.com>

--- modelkit/core/models/tensorflow_model.py
+++ modelkit/core/models/tensorflow_model.py
@@ -238,6 +238,41 @@ class TensorflowModel(Model[ItemType, ReturnType], TensorflowModelMixin):
             for name in self.output_tensor_mapping
         }
 
+    def get_keras_model(self):
+        class LayerFromSavedModel(tf.keras.layers.Layer):
+            """
+            Builds a keras Layer from a saved Tensorflow model
+            """
+
+            def __init__(self, saved_model_signature):
+                super().__init__()
+                self.saved_model_signature = saved_model_signature
+
+            def call(self, inputs):
+                return self.saved_model_signature(
+                    **{
+                        key: tf.convert_to_tensor(value)
+                        for key, value in inputs.items()
+                    }
+                )
+
+        class KerasModelFromSavedModel(tf.keras.Model):
+            """
+            Builds a keras Model from a saved Tensorflow model
+            via an artificial keras Layer built from the saved model
+            signature.
+            """
+
+            def __init__(self, saved_model_signature):
+                super().__init__()
+                self.layer = LayerFromSavedModel(saved_model_signature)
+
+            def call(self, inputs):
+                out = self.layer(inputs)
+                return out
+
+        return KerasModelFromSavedModel(self.tf_model_signature)
+
     def close(self):
         if self.requests_session:
             return self.requests_session.close()
--- tests/test_tf_model.py
+++ tests/test_tf_model.py
@@ -141,6 +141,20 @@ def test_tf_model_local_path(dummy_tf_models):
     assert np.allclose(v, model({"input_1": v})["lambda"])
 
 
+@skip_unless("ENABLE_TF_TEST", "True")
+def test_tf_keras_model(dummy_tf_models):
+    DummyTFModel, *_ = dummy_tf_models
+    model = DummyTFModel(
+        asset_path=os.path.join(TEST_DIR, "testdata", "dummy_tf_model", "0.0"),
+        model_settings={
+            "output_dtypes": {"lambda": np.float32},
+            "output_tensor_mapping": {"lambda": "nothing"},
+            "output_shapes": {"lambda": (3, 2, 1)},
+        },
+    )
+    assert model.get_keras_model()
+
+
 @skip_unless("ENABLE_TF_SERVING_TEST", "True")
 @skip_unless("ENABLE_TF_TEST", "True")
 def test_tf_model(monkeypatch, working_dir, dummy_tf_models):
