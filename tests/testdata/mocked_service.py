39b789eef463a450c61675965fa4046fb8287f73
Nicolas Carrez <ncarrez@csod.com>
2023-09-28 16:52:04 +0200

    distant http: prepare tests for header support

--- tests/testdata/mocked_service.py
+++ tests/testdata/mocked_service.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Optional, Union
 
 import fastapi
 from starlette.responses import JSONResponse
+from typing_extensions import Annotated
 
 app = fastapi.FastAPI(title="Mock service")
 
@@ -11,7 +12,10 @@ async def some_endpoint(
     item: Dict[str, Union[str, int]],
     limit: Optional[int] = None,
     skip: Optional[int] = None,
+    x_correlation_id: Annotated[Union[str, None], fastapi.Header()] = None,
 ):
+    if x_correlation_id:
+        item["x_correlation_id"] = x_correlation_id
     if limit:
         item["limit"] = limit
     if skip:
@@ -24,8 +28,11 @@ async def some_endpoint_batch(
     items: List[Dict[str, Union[str, int]]],
     limit: Optional[int] = None,
     skip: Optional[int] = None,
+    x_correlation_id: Annotated[Union[str, None], fastapi.Header()] = None,
 ):
     for item in items:
+        if x_correlation_id:
+            item["x_correlation_id"] = x_correlation_id
         if limit:
             item["limit"] = limit
         if skip:
