1a2764c8ca64e7b9b43aef44c0f39484ea10d4b0
Thomas Genin <6623268+tgenin@users.noreply.github.com>
2024-06-05 15:18:41 +0200

    Fix azure list_blobs deprecated arg

--- modelkit/assets/drivers/azure.py
+++ modelkit/assets/drivers/azure.py
@@ -55,7 +55,7 @@ class AzureStorageDriver(StorageDriver):
     @retry(**AZURE_RETRY_POLICY)
     def iterate_objects(self, prefix=None):
         container = self.client.get_container_client(self.bucket)
-        for blob in container.list_blobs(prefix=prefix):
+        for blob in container.list_blobs(name_starts_with=prefix):
             yield blob["name"]
 
     @retry(**AZURE_RETRY_POLICY)
